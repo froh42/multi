09a78d5054695e6d1a7794cd5b5eaeecab49acf5
Florian Sesser <fs@it-agenten.com>
2012-06-28 23:04:36 +0200

    test.js with code copypasta from https://github.com/Camme/webdriverjs
    Works on my local machine with a selenium server started like
    $ java -jar selenium-server-standalone-2.24.1.jar

---
+++ selenium/test.js
@@ -0,0 +1,17 @@
+// FS: This is copypasta from
+// https://github.com/Camme/webdriverjs
+
+var webdriverjs = require("webdriverjs");
+var client = webdriverjs.remote();
+//var client = webdriverjs.remote({host: "xx.xx.xx.xx"}); // to run it on a remote webdriver/selenium server
+//var client = webdriverjs.remote({desiredCapabilities:{browserName:"chrome"}}); // to run in chrome
+
+client
+    .init()
+    .url("https://github.com/")
+    .getElementSize("id", "header", function(result){ console.log(result);  })
+    .getTitle(function(title) { console.log(title) })
+    .getElementCssProperty("id", "header", "color", function(result){ console.log(result);  })
+    .end(); 
+
+
