314d22b10166fc84091364c2b3bc4a1ac811b11d
Florian Sesser <fs@it-agenten.com>
2012-07-02 01:55:55 +0200

    Selenium WebdriverJS test: Solve a mult exercise
    OMGWTF WebdriverJS has a scheduler that queues all
    my commands as promises. I cannot easily have state.
    I trick myself out of this by using the result field
    as accumulator. Ugly!

---
+++ selenium/02_solve_some_mults.js
@@ -0,0 +1,73 @@
+// Test: Transition from start- to game screen
+
+var assert = require("assert"),
+    webdriver = require("./lib/webdriver");
+
+var client = new webdriver.Builder().
+//    usingServer('http://localhost:4444/wd/hub').
+    withCapabilities({
+      'browserName': 'firefox',
+      'version': '',
+      'platform': 'ANY',
+      'javascriptEnabled': true
+    }).
+    build();
+
+
+// Construct URL to local file.
+
+var test_url = "file://" + __dirname + "/../src/start.html";
+client.get(test_url);
+
+
+// Click through to next page.
+
+client.findElement(webdriver.By.linkText("Start game")).click();
+
+
+// Solve a mult exercise
+
+// Uuaagh: We cannot hold state well in this async-o-rama,
+// so I copy the factors into the result field, eval() (!!)
+// it's content and send the result back to the field.
+
+var field = client.findElement(webdriver.By.xpath("//input[@data-bind='value: result']"));
+field.clear();
+
+client
+    .findElement(webdriver.By.xpath("//span[@data-bind='text: factor1']"))
+    .getText()
+    .then(function(text) {
+	field.sendKeys(text + " * ");
+	//console.log(text);
+    });
+
+client
+    .findElement(webdriver.By.xpath("//span[@data-bind='text: factor2']"))
+    .getText()
+    .then(function(text) {
+	field.sendKeys(text);
+	//console.log(text);
+    });
+
+field
+    .getAttribute("value")
+    .then(function(text) {
+	result = eval(text);
+	console.log(text + " = " + result);
+	field.clear();
+	field.sendKeys(result);
+    });
+
+
+// For the world to see!
+client.sleep(2000);
+
+// Now we should probably click on a button or something
+client
+    .findElement(webdriver.By.linkText("Next exercise"))
+    .click();
+
+client.sleep(2000);
+client.quit();
+
