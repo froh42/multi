38c69040539b441217afaa7ede63dc44646d990a
Frohwalt Egerer <git@frohwalt.de>
2012-07-01 17:40:19 +0200

    Fix stupid test script bug

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 (
     cd jsTestDriver
+    echo "***** SERVER STOP"
     ./server.sh stop
+    echo "***** SERVER START"
     ./server.sh start
     sleep 1
-    tail -f *.err *.out
+    tail -f *.err *.out &
+    sleep 1
+    echo "***** RUN TEST"
     ./runtests.sh --config ../test.jstd --tests all --testOutput ../testReports
+    echo "***** SERVER STOP"
     ./server.sh stop
 )
 
