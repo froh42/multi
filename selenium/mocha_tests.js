33667692eceff0394a42d530e207c1176a55ab29
Florian Sesser <fs@it-agenten.com>
2012-07-02 03:26:48 +0200

    Roll all selenium tests into a mocha suite
    Posh!
    
    Start with sth. like:
    ../node_modules/.bin/mocha -t 30000 --reporter spec mocha_tests.js

--- selenium/mocha_tests.js
+++ selenium/mocha_tests.js
@@ -1,4 +1,4 @@
-// Test: Transition from start- to game screen
+// Mocha / Selenium Test suite for multi
 // Depends: Selenium webdriver.js and mocha test runner
 
 var assert = require("assert"),
@@ -17,35 +17,80 @@ var client = new webdriver.Builder()
 // Construct URL to local file.
 var test_url = "file://" + __dirname + "/../src/start.html";
 
+
+
 describe("Run Selenium tests", function() {
 
     before(function(done) {
-	client
-	    .get(test_url);
+        client
+            .get(test_url);
 
-	client
-	    .findElement(webdriver.By.css("body"))
-	    .then(function(){done()});
+        client
+            .findElement(webdriver.By.css("body"))
+            .then(function(){done()});
     });
 
-    //beforeEach(function(done) {
-    //    // Navigate to the URL for each test
-    //    // client.get(test_url, done);
-    //    done();
-    //});
+    after(function(done) {
+        client
+            .quit()
+            .then(function(){done();});
+    });
 
     describe('The start page', function() {
         it('should have a `Start game` link', function() {
-	    assert.ok(client.findElement(webdriver.By.linkText("Start game")));
+            assert.ok(client.findElement(webdriver.By.linkText("Start game")));
         });
     });
 
     describe('The `Start game` link', function() {
-	it('should lead us to the game page', function(done) {
-	    client.findElement(webdriver.By.linkText("Start game")).click();
-	    assert.ok(client.findElement(webdriver.By.linkText("Next exercise")).then(function(){done();}));
-	});
+        it('should lead us to the game page', function(done) {
+            client.findElement(webdriver.By.linkText("Start game")).click();
+            assert.ok(client.findElement(webdriver.By.linkText("Next exercise")).then(function(){done();}));
+        });
+    });
+
+    describe('The game page', function() {
+        it('should have a nice multiplication exercise for us to solve', function(done) {
+            // Solve a mult exercise
+            
+            // Uuaagh: We cannot hold state well in this async-o-rama,
+            // so I copy the factors into the result field, eval() (!!)
+            // its content and send the result back to the field.
+            
+            var field = client.findElement(webdriver.By.xpath("//input[@data-bind='value: result']"));
+            field.clear();
+            
+            client
+                .findElement(webdriver.By.xpath("//span[@data-bind='text: factor1']"))
+                .getText()
+                .then(function(text) {
+                    field.sendKeys(text + " * ");
+                    //console.log(text);
+                });
+            
+            client
+                .findElement(webdriver.By.xpath("//span[@data-bind='text: factor2']"))
+                .getText()
+                .then(function(text) {
+                    field.sendKeys(text);
+                    //console.log(text);
+                });
+            
+            field
+                .getAttribute("value")
+                .then(function(text) {
+                    var result = eval(text);
+                    console.log(text + " = " + result);
+                    field.clear();
+                    field.sendKeys(result);
+                    done();
+                });
+        });
+
+        it('should be kept open for a couple more seconds', function(done) {
+            // For the world to see!
+            client.sleep(5000).then(function(){done();});
+        });
     });
 });
-
 
