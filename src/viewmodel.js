9ca85656c3398eda7c7397da91a15da9febd4fa0
Frohwalt Egerer <mail@frohwalt.de>
2012-07-04 20:16:42 +0200

    Nur Aufgaben ab 2

--- src/viewmodel.js
+++ src/viewmodel.js
@@ -51,8 +51,8 @@ function ExerciseModel(gameModel) {
     this.gameModel = gameModel;
     this.exerciseNumber = ko.observable(1);
     this.exerciseCount = ko.observable(10);
-    this.factor1 = ko.observable(getRandomInt(1, 10));
-    this.factor2 = ko.observable(getRandomInt(1, 10));
+    this.factor1 = ko.observable(getRandomInt(2, 10));
+    this.factor2 = ko.observable(getRandomInt(2, 10));
     this.result = ko.observable("");
     this.resultIsCorrect = ko.computed(function () {
         return parseInt(this.result(), 10) == parseInt(this.factor1(), 10) * parseInt(this.factor2(), 10);
