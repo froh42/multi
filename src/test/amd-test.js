b07a3eb2a890dd40f638968140e51f12746ce8a0
Frohwalt Egerer <mail@frohwalt.de>
2012-08-29 00:22:00 +0200

    Add simple test as in buster-amd example

--- src/test/amd-test.js
+++ src/test/amd-test.js
@@ -1,25 +1,25 @@
-define(["exerciseModel"], function(xxx) {
+define(["testModule"], function(mod) {
 	buster.testCase("viewmodelTest", {
 		"test resultCorrect is true when result = factor1*factor2": function() {
-			var model = new xxx.ExerciseModel();
+			var model = new mod.ExerciseModel();
 			model.factor1(3);
 			model.factor2(9);
 			model.result(3 * 9);
 			assert(model.resultIsCorrect());
 		},
 		"test resultCorrect is false when result != factor1*factor2": function() {
-			var model = new xxx.ExerciseModel();
+			var model = new mod.ExerciseModel();
 			model.factor1(3);
 			model.factor2(7);
 			model.result(3 * 9);
 			refute(model.resultIsCorrect());
 		},
 		"test ExerciseModel demands 10 exercises": function() {
-			var model = new xxx.ExerciseModel();
+			var model = new mod.ExerciseModel();
 			assert(10, model.exerciseCount());
 		},
 		"test ExerciseModel starts at exercise 1": function() {
-			var model = new xxx.ExerciseModel();
+			var model = new mod.ExerciseModel();
 			assert.equals(1, model.exerciseNumber());
 		}
 	});
