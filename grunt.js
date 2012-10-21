962861873a18dcdf1893b4afe1eb62d397a52765
Florian Sesser <florian.sesser@web.de>
2012-10-21 23:38:17 +0300

    Update grunt.js
    Typo; Also: Try out Github Online Code Editor

--- grunt.js
+++ grunt.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
     // Default task to be run during development
     grunt.registerTask('default', 'lint browserTest zombie');
 
-    // Tasks that are are to slow for interactive use
+    // Tasks that are to slow for interactive use
     grunt.registerTask('molasses', 'selenium');
 
     // Tasks run on npm install (travics-ci)
