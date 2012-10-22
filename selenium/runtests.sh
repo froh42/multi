8e18efbd4f5ca3563eba9b5f5e6612d963ea6661
Florian Sesser <fs@it-agenten.com>
2012-10-23 00:50:50 +0200

    Use local mocha.js binary (instead of relying it to be in the $PATH)

--- selenium/runtests.sh
+++ selenium/runtests.sh
@@ -5,6 +5,6 @@ set -e
 ROOTDIR="$( cd "$( dirname "$0")" && pwd )"
 
 cd $ROOTDIR
-mocha -t 120000 --reporter spec testsuite.js
+../node_modules/.bin/mocha -t 120000 --reporter spec testsuite.js
 
 exit $?
